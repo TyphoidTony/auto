f3acfb5fcdb571a9af5d0a09f7d8a5a98da69f1e
Anthony Soto <stuthagen@gmail.com>
2016-05-24 18:36:53 -0400

    added more options and restructered file completely will update README to reflect changes

--- auto.sh
+++ auto.sh
@@ -1,23 +1,39 @@
+#!/bin/bash
 # NOTE there is no input checking, since I made this for myself I didn't bother to include it. 
-#TODO add a long-ish delay after a set amount of actions . maybe 3-5 minutes after 150 actions. i.e. fake afk.
+#TODO add a long-ish delay after a set amount of actions . maybe 3-5 minutes after 150 actions. i.e. fake afk. 
+#long delay can at the moment be done by the user by hitting ctrl+z to 
 #TODO send input to multiple windows at the same time. Or at least seemingly at the same time.
+#DONE: added user selected range for shuf command. no longer need to go into file to modify range for specific function.
+#TODO add the ability for the mouse to get current loction of mouse before xdotool activates selected x,y loction and sends keypress
+#This way you don't need to move the mouse back to the original loction. 
+#TODO 
+
 
 clicker(){
 
     while true;
     do
-	rand=$(shuf -i  57-100 -n 1);
+	x="$1";#x loction on the screen that you want pressed.
+	y="$2";#y loction
+	w="$3"; #name of window
+	r1="$4";#starting range for shuf
+	r2="$5";#ending range for shuf
 
-	x="$1";
-	y="$2";
-	w="$3"
+	rand=$(shuf -i $r1-$r2  -n 1);
+
+
 	echo $rand;
 
-	wurm=$(xdotool search $w windowactivate --sync );
+	window=$(xdotool search  $w  | head -1 );
+	#Not really needed but useful to keep incase I find a use for
+	#a variable that holds the current window.
+	xdotool  windowactivate  $window
+	xdotool  windowactivate  $window
+	xdotool  windowactivate  $window
+	xdotool  windowactivate  $window
+	xdotool mousemove $x $y;
 
-	xdotool key --clearmodifiers  mousemove $x $y;##use xdotool getmouselocation to get the loction of the "continue" Button 
-
-	xdotool key --clearmodifiers  click 1;
+	xdotool   click 1;
 
 	sleep $rand;
 
@@ -25,29 +41,32 @@ clicker(){
 
 }
 
-presskey(){
+presskey() {
     while true;
     do 
-	key="$1";#put key you want pressed three times here.
-	w = "$2"
-	rand=$(shuf -i 58-99  -n 1);
+	key="$1";#key being pressed
+	w="$2";#name of the window to send key presses to.
+	r1="$3";#starting range for shuf
+	r2="$4";#ending range for shuf
+	rand=$(shuf -i $r1-$r2  -n 1);
 
 	echo $rand;
 
-	wurm=$(xdotool search --onlyvisible --class $w | head -1);
+	window=$(xdotool search  --name $w | head -1);
 
-	xdotool key --clearmodifiers -window $wurm $key;
-	sleep 1
-	xdotool key --clearmodifiers -window $wurm $key;
-	sleep 1
-	xdotool key --clearmodifiers -window $wurm $key;
-	sleep 1
+
+	xdotool key --clearmodifiers -window $window $key;
+	sleep 1;
+	xdotool key --clearmodifiers -window $window $key;
+	sleep 1;
+	xdotool key --clearmodifiers -window $window $key;
+	sleep 1;
 	sleep $rand;
 
-    done
+    done;
 }
 
-getpos(){
+getpos() {
 
 
     while true 
@@ -68,33 +87,56 @@ getpos(){
     done
 
 }
-
+############## START OF MAIN BLOCK########################
 echo "What is the name of the window you want to control?"
 read window
 
 echo "Do you want a (c)licker or a (k)ey presser?"
 read input
 
-if [[ "$input" = "c" ]];
+
+#### CLICKER OPTION ####
+if [[ "$input" = "c" ]]
 then 
 
     getpos
     
+
     echo "What x position do you want the mouse to click?"
     read xpos
 
     echo "What y position do you want the mouse to click?"
     read ypos
 
-    clicker $xpos $ypos $window
+
+    echo "what range do you want the generator to be between?"
+
+    echo "starting range:"
+    read range1 
+
+    echo "max range:"
+    read range2
+
+    clicker $xpos $ypos $window $range1 $range2
 fi
 
+
+#### KEY PRESSER OPTION ####
 if [ "$input" = "k" ]
 then 
 
     echo "Enter key combo you want to press e.g. ctrl+c";
     read keyPress;
-    presskey $keyPress $window
+    #TODO get this working
+    echo "What range do you want the generator to be between?";
+	
+    echo "starting range:";
+    read range1; 
+	   
+    echo "max range:";
+    read range2;
+
+    presskey $keyPress $window $range1 $range2
 fi
 
-
+################## END OF MAIN BLOCK#######################
